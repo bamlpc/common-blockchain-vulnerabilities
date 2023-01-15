405e964b2c9c308fdcc9b665003e204e87356a82
bmogetta <brunomogetta@gmail.com>
2023-01-15 11:42:26 -0300

    ci: trying ensure sync

--- .github/scripts/src/store_new_cbv_in_folder.ts
+++ .github/scripts/src/store_new_cbv_in_folder.ts
@@ -1,4 +1,4 @@
-import { ensureDir } from 'https://deno.land/std@0.171.0/fs/mod.ts';
+import { ensureDirSync } from 'https://deno.land/std@0.171.0/fs/mod.ts';
 import { CBV } from './schemas.ts';
 
 async function store_new_cbv_in_folder(
@@ -8,11 +8,11 @@ async function store_new_cbv_in_folder(
 ) {
 	const get_subfolders: Array<string> = _cbv_obj.blockchain.split(', ') ||
 		[_cbv_obj.blockchain];
-	//TODO: SPLIT THIS 2 FOR AWAIT IN TO TWO DIFFERENT FUNCTIONS
-	// create any blockchain folder that doesn't exist
-	const folders = await ensure_folder_exist(get_subfolders);
-	// store CBV in all it's corresponding directories
 
+	// create any blockchain folder that doesn't exist TODO: figure it out why this work only locally
+	const folders = await ensure_folder_exist(get_subfolders);
+	
+	// store CBV in all it's corresponding directories
 	if ( folders === "success") {
 		for await (const subfolder of get_subfolders) {
 			const sub_folder_path = `${Deno.cwd()}/issues/${subfolder}`;
@@ -30,7 +30,7 @@ async function ensure_folder_exist(sub_folders_needed:Array<string>) {
 	try {
 		for await (const subfolder of sub_folders_needed) {
 			const folder_path = `${Deno.cwd()}/issues/${subfolder}`;
-			ensureDir(folder_path);
+			ensureDirSync(folder_path);
 		}
 		return "success"
 	} catch (error) {
