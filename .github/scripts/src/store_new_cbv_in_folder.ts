a14216947294eaf50c45fe12289b4b850e70a5fc
bmogetta <brunomogetta@gmail.com>
2023-01-15 11:44:40 -0300

    ci: ensureDirSync

--- .github/scripts/src/store_new_cbv_in_folder.ts
+++ .github/scripts/src/store_new_cbv_in_folder.ts
@@ -6,35 +6,20 @@ async function store_new_cbv_in_folder(
 	_cbv_ready_to_be_stored: string,
 	_cbv_obj: CBV,
 ) {
-	const get_subfolders: Array<string> = _cbv_obj.blockchain.split(', ') ||
-		[_cbv_obj.blockchain];
-
-	// create any blockchain folder that doesn't exist TODO: figure it out why this work only locally
-	const folders = await ensure_folder_exist(get_subfolders);
+	const get_subfolders: Array<string> = _cbv_obj.blockchain.split(', ');
 	
-	// store CBV in all it's corresponding directories
-	if ( folders === "success") {
-		for await (const subfolder of get_subfolders) {
-			const sub_folder_path = `${Deno.cwd()}/issues/${subfolder}`;
-			await Deno.writeTextFile(
-				`${sub_folder_path}/${_new_cbv_code_name}.md`,
-				_cbv_ready_to_be_stored,
-				);
-			}
-	} else {
-		throw new Error("Cannot ensure folder structure")
+	// create any blockchain folder that doesn't exist
+	for await (const subfolder of get_subfolders) {
+		const folder_path = `${Deno.cwd()}/issues/${subfolder}`;
+		ensureDirSync(folder_path);
 	}
-}
-
-async function ensure_folder_exist(sub_folders_needed:Array<string>) {
-	try {
-		for await (const subfolder of sub_folders_needed) {
-			const folder_path = `${Deno.cwd()}/issues/${subfolder}`;
-			ensureDirSync(folder_path);
-		}
-		return "success"
-	} catch (error) {
-		throw error
+	// store CBV in all it's corresponding directories
+	for await (const subfolder of get_subfolders) {
+		const sub_folder_path = `${Deno.cwd()}/issues/${subfolder}`;
+		await Deno.writeTextFile(
+			`${sub_folder_path}/${_new_cbv_code_name}.md`,
+			_cbv_ready_to_be_stored,
+		);
 	}
 }
 
