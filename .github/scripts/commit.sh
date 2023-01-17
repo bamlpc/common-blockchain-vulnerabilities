0c50d0805b208dde2cb26aa7c8ff06b6fa108ba6
Bruno Mogetta <brunomogetta@gmail.com>
2023-01-17 11:16:57 -0300

    ci: fix Permission denied error

--- .github/scripts/commit.sh
+++ .github/scripts/commit.sh
@@ -2,13 +2,10 @@
 { IFS= read -rd '' value <DENO_OUTPUT.txt;} 2>/dev/null
 echo "print value"
 echo $value
-str=`$value`
-echo "print string"
-echo $str
-name=`echo "${str:11-1}"`
-echo "print name"
+echo grep -oP "(?<=\>)\w{3}-{1}\d{2}-{1}\d{5}(?=<)" $value > name.txt
+name=`cat name.txt`
+echo "name"
 echo $name
-
 
 
 git config --local user.email "action@github.com"
